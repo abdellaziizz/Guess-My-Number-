fe1116b61beb899e0b61e60670997c5d41097f38
ahmed abdelaziz <a.abdelaziz2338@gmail.com>
2026-07-03 23:04:18 +0300

    adding the high score . almost finsh the project

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 const SecretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highScore = 0;
 function displayMessage(message) {
   console.log(` ${message} `);
 }
@@ -39,5 +40,18 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.message').textContent = 'Correct Number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (highScore < score) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent = 'Start guessing...';
 });
